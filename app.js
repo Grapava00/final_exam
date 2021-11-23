bfe9c67862870888a35a2656cb4b1c01d37b282b
Grapava00 <85571784+Grapava00@users.noreply.github.com>
2021-11-23 22:50:11 +0400

    update responsive

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ let images = [];
 let time = 5000;
 
 images[0] = "images/personal_photos_welcome/tamu1.png";
-images[1] = "images/personal_photos_welcome/tamu2.png";
-images[2] = "images/personal_photos_welcome/tamu3.png";
+images[1] = "images/personal_photos_welcome/tamu3.png";
 function changeImg() {
 	document.slide.src = images[i];
 
