0febd5929955f641fa209d1eabf4ea1824458a28
Grapava00 <85571784+Grapava00@users.noreply.github.com>
2021-11-26 15:21:20 +0400

    update style

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ let i = 0;
 let images = [];
 let time = 5000;
 
-images[0] = "images/personal_photos_welcome/tamu1.png";
-images[1] = "images/personal_photos_welcome/tamu3.png";
+
+images[0]=  "images/personal_photos_introduction/my-picture2.png";
+images[1]=  "images/personal_photos_introduction/my-picture3.png";
+
 function changeImg() {
 	document.slide.src = images[i];
-
+    
 	if (i < images.length - 1) {
 		i++;
 	} else {
