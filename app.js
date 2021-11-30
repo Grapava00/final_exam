403fa78ca381ba7ef6f865909c626f64ef2237dd
Grapava00 <Grapava00@gmail.com>
2021-12-01 00:24:35 +0400

    add comment section

--- app.js
+++ app.js
@@ -56,3 +56,32 @@ window.addEventListener("scroll", () => {
 		hideProgress();
 	}
 })
+
+// slide-comment
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName("mySlides");
+	let dots = document.getElementsByClassName("fa-square");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "flex";
+	dots[slideIndex - 1].className += " active";
+}
